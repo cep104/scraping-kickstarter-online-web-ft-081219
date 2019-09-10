4a9e05a89ff3f7da2fc36ecb32c7a327c50d98ea
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-10 04:21:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
 kickstarter = Nokogiri::HTML(html)
 projects = {}
 kickstarter.css("li.project.grid_4").each do |project| 
-  title = 
+  title = project.css("h2.bbcard_name strong a").text
   projects[project] = {}
 end
 projects
