379b2af987d83f09671b8d76dbf857d6bb1f0d70
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-10 04:07:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,19 @@
 # require libraries/modules here
 
+require "nokogiri"
+require 'pry'
+
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+ 
+kickstarter = Nokogiri::HTML(html)
+
+project =
+
+binding.pry
 end
+create_project_hash
+
+# projects: kickstarter.css("li.project.grid_4")
+# project.css("h2.bbcard_name strong a").text = title
+
